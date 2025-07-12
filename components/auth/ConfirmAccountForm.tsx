adf435846e5f103d457b6c36bb758fb4664a896b
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-12 09:03:28 -0600

    feat: add toast component

--- components/auth/ConfirmAccountForm.tsx
+++ components/auth/ConfirmAccountForm.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { confirmAccount } from "@/actions/confirm-account-action"
 import { PinInput, PinInputField } from "@chakra-ui/pin-input"
+import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 import { useFormState } from "react-dom"
-import ErrorMessage from "../ui/ErrorMessage"
-import SuccessMessage from "../ui/SuccessMessage"
+import { toast } from "react-toastify"
 
 export default function ConfirmAccountForm() {
+  const router = useRouter()
   const [isComplete, setIsComplete] = useState(false)
   const [token, setToken] = useState("")
   const confirmAccountWithToken = confirmAccount.bind(null, token)
@@ -21,7 +22,23 @@ export default function ConfirmAccountForm() {
     }
   }, [isComplete])
 
+  useEffect(() => {
+    if (state.errors) {
+      state.errors.forEach(error =>
+        toast.error(error)
+      )
+    }
+    if (state.success) {
+      toast.success(state.success, {
+        onClose: () => {
+          router.push('/auth/login')
+        }
+      })
+    }
+  }, [state])
+
   const handleChange = (token: string) => {
+    setIsComplete(false)
     setToken(token)
   }
 
@@ -32,8 +49,6 @@ export default function ConfirmAccountForm() {
   return (
 
     <>
-      {state.errors.map(error => <ErrorMessage>{ error }</ErrorMessage>)}
-      {state.success && <SuccessMessage>{ state.success }</SuccessMessage>}
       <div className="flex justify-center gap-5 my-10">
         <PinInput
           value={token}
@@ -48,6 +63,6 @@ export default function ConfirmAccountForm() {
           <PinInputField className="h-10 w-10 border border-gray-300 shadow text-center rounded-lg placeholder-white" />
         </PinInput>
       </div>
-    </>  
+    </>
   )
 }
