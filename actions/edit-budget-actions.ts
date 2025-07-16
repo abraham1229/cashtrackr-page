05b4635fa6e9be3d5fa966c555deabe5bb007205
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-15 19:13:26 -0600

    feat: add conection to db on budget edition

---
+++ actions/edit-budget-actions.ts
@@ -0,0 +1,57 @@
+"use server"
+
+import getToken from "@/src/auth/token"
+import { Budget, DraftBudgetSchema, ErrorResponseSchema, SucessSchema } from "@/src/schemas"
+
+type ActionStateType = {
+  errors: string[],
+  success: string
+}
+
+export async function editBudget(budgetId: Budget['id'], prevState: ActionStateType, formData: FormData) {
+  
+  const budgetData = {
+    name: formData.get('name'),
+    amount: formData.get('amount'),
+  }
+
+  const budget = DraftBudgetSchema.safeParse(budgetData)
+
+  if (!budget.success) {
+    return {
+      errors: budget.error.errors.map(error => error.message),
+      success: ''
+    }
+  }
+
+  const token = getToken()
+  const url = `${process.env.API_URL}/budgets/${budgetId}`
+  const res = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({
+      name: budget.data.name,
+      amount: budget.data.amount,
+    })
+  })
+
+  const json = await res.json()
+
+  if (!res.ok) {
+    const { error } = ErrorResponseSchema.parse(json)
+    return {
+      errors: [error],
+      success: ''
+    }
+  }
+
+  const success = SucessSchema.parse(json)
+
+  return {
+    errors: [],
+    success: success
+  }
+}
--- components/budgets/EditBudgetForm.tsx
+++ components/budgets/EditBudgetForm.tsx
@@ -1,13 +1,39 @@
 'use client'
 import { Budget } from "@/src/schemas";
 import BudgetForm from "./BudgetForm";
+import { useFormState } from "react-dom";
+import { editBudget } from "@/actions/edit-budget-actions";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
-export default function EditBudgetForm({budget}:{budget: Budget}) {
+export default function EditBudgetForm({ budget }: { budget: Budget }) {
+
+  const router = useRouter()
+  const editBudgetWithId = editBudget.bind(null, budget.id)
+
+  const [state, dispatch] = useFormState(editBudgetWithId, {
+    errors: [],
+    success: ''
+  })
+
+  useEffect(() => {
+    if (state.errors) {
+      state.errors.forEach(error => {
+        toast.error(error)
+      })
+    }
+    if (state.success) {
+      toast.success(state.success)
+      router.push('/admin')
+    }
+  }, [state])
+
   return (
     <form
       className="mt-10 space-y-3"
       noValidate
-      action={() => {}}
+      action={dispatch}
     >
       <BudgetForm
         budget={budget}
