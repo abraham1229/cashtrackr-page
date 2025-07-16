779d5dd8e139e710a67a949e313afb04ea8a91a6
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-15 18:51:08 -0600

    feat: populate info when editing

--- components/budgets/BudgetForm.tsx
+++ components/budgets/BudgetForm.tsx
@@ -1,5 +1,6 @@
+import { Budget } from "@/src/schemas";
 
-export default function BudgetForm() {
+export default function BudgetForm({budget}:{budget?: Budget}) {
   return (
     <>
       <div className="space-y-3">
@@ -12,6 +13,7 @@ export default function BudgetForm() {
           type="text"
           placeholder="vacations"
           name="name"
+          defaultValue={budget?.name}
         />
       </div>
       <div className="space-y-3">
@@ -24,6 +26,7 @@ export default function BudgetForm() {
           className="w-full p-3  border border-gray-100 bg-slate-100"
           placeholder="$100"
           name="amount"
+          defaultValue={budget?.amount}
         />
       </div>
     </>
