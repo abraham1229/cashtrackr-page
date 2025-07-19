3988646056d711ab4d53fc8495a9ccb528f3164e
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-19 17:54:21 -0600

    feat: add profile layout and navigation

---
+++ app/admin/profile/password/page.tsx
@@ -0,0 +1,11 @@
+
+export default async function ChangePasswordPage() {
+  return (
+    <>
+      <h1 className="font-black text-4xl text-purple-950 my-5">Change Password</h1>
+      <p className="text-xl font-bold">Here you can change your {''}
+        <span className="text-amber-500">password</span>
+      </p>
+    </>
+  )
+}
---
+++ app/admin/profile/settings/page.tsx
@@ -0,0 +1,11 @@
+
+export default async function EditProfilePage() {
+  return (
+    <>
+      <h1 className="font-black text-4xl text-purple-950 my-5">Update Profile</h1>
+      <p className="text-xl font-bold">Here you can update your {''}
+        <span className="text-amber-500">profile info</span>
+      </p>
+    </>
+  )
+}
